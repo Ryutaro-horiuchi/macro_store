7216faf08c49094ee44e562ca3aa022a258ce46b
yorutaru <rq-yorutaru-at-321899771753>
2021-09-13 16:19:19 +0900

    [Clean]Top .vueファイル名に変更

--- app/javascript/pages/Top.vue
+++ app/javascript/pages/Top.vue
@@ -8,9 +8,7 @@
 </template>
 
 <script>
-
 </script>
 
 <style scoped>
-
 </style>
--- app/javascript/router/router.js
+++ app/javascript/router/router.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import TopIndex from '../pages/top/index.vue'
+import TopPage from '../pages/Top.vue'
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  routes: [{path: '/', component: TopIndex}]
+  routes: [{path: '/', component: TopPage}]
 })
