f89636f7c42bd37fe1c181c488513ce1224ecf3c
yorutaru <rq-yorutaru-at-321899771753>
2021-11-21 17:11:14 +0900

    [Add]カロリー計算ページのレイアウト実装#13

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import top from '../pages/top/index.vue';
 import register from '../pages/user/register/index.vue';
 import login from '../pages/user/login/index.vue';
+import calorie from '../pages/user/calorie/index.vue';
 import search from '../pages/food/search.vue';
 import food from '../pages/food/index.vue'
 import confirmation from '../pages/food/confirmation.vue';
@@ -14,6 +15,7 @@ export default new Router({
   routes: [{path: '/', component: top},
            {path: '/signup', component: register},
            {path: '/login', component: login},
+           {path: '/calorie', component: calorie},
            {path: '/search', component: search},
            {path: '/foods', component: food},
            {path: '/confirmation', component: confirmation}
