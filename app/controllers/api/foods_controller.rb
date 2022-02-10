087c401e8d60371a561d462543105b3a474f95e0
yorutaru <rq-yorutaru-at-321899771753>
2022-02-10 13:03:12 +0900

    [Clean]rails側の検索ロジックのリファクタリング#134

--- app/controllers/api/foods_controller.rb
+++ app/controllers/api/foods_controller.rb
@@ -1,47 +1,17 @@
 class Api::FoodsController < ApplicationController
   def search_name
     foods = Food.where('name like ?', "%#{params[:name]}%")
-    render_foods(foods)
-  end
-
-  def search_nutrient
-    foods = search_protein
-    if foods.present?
-      foods = search_carbohydrate(foods)
-      foods = search_lipid(foods)
-    end
-    render_foods(foods)
-  end
-
-  private
-
-  def render_foods(foods)
     render json: foods
   end
 
-  def search_protein
-    if nutrient_params[:proteinMaximum].present?
-      Food.where(protein: nutrient_params[:proteinMinimum]..nutrient_params[:proteinMaximum])
-    else
-      Food.where(protein: nutrient_params[:proteinMinimum]..)
-    end
+  def search_nutrient
+    foods = Food.protein_bitween(nutrient_params[:proteinMinimum], nutrient_params[:proteinMaximum]).
+            carbohydrate_bitween(nutrient_params[:carbohydrateMinimum], nutrient_params[:carbohydrateMaximum]).
+            lipid_bitween(nutrient_params[:lipidMinimum], nutrient_params[:lipidMaximum])
+    render json: foods
   end
 
-  def search_carbohydrate(foods)
-    if nutrient_params[:carbohydrateMaximum].present?
-      foods.where(carbohydrate: nutrient_params[:carbohydrateMinimum]..nutrient_params[:carbohydrateMaximum])
-    else
-      foods.where(carbohydrate: nutrient_params[:carbohydrateMinimum]..)
-    end
-  end
-
-  def search_lipid(foods)
-    if nutrient_params[:lipidMaximum].present?
-      foods.where(lipid: nutrient_params[:lipidMinimum]..nutrient_params[:lipidMaximum])
-    else
-      foods.where(lipid: nutrient_params[:lipidMinimum]..)
-    end
-  end
+  private
 
   def nutrient_params
     params.permit(:proteinMinimum, :proteinMaximum, :carbohydrateMinimum, :carbohydrateMaximum, :lipidMinimum, :lipidMaximum)
--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,14 @@ class Food < ApplicationRecord
   mount_uploader :image, ImageUploader
   enum store: { seven_eleven: 0, family_mart: 1, lawson: 2 }
   has_many :bookmarks, dependent: :destroy
+
+  scope :protein_bitween, -> minimum, maximum { 
+    maximum.present? ? where(protein: minimum..maximum) : where(protein: minimum..)
+  }
+  scope :carbohydrate_bitween, -> minimum, maximum { 
+    maximum.present? ? where(carbohydrate: minimum..maximum) : where(carbohydrate: minimum..)
+  }
+  scope :lipid_bitween, -> minimum, maximum { 
+    maximum.present? ? where(lipid: minimum..maximum) : where(lipid: minimum..)
+  }
 end
